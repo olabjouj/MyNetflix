b0bd60d9cbc0f6733c6cfeebf1c42092c9a34254
Oumayma LABJOUJ <oumayma.labjouj@gmail.com>
2021-02-09 16:30:10 +0100

    my second commit

--- src/container/App.js
+++ src/container/App.js
@@ -9,13 +9,15 @@ import Video from '../componemts/videos';
 const API_END_POINT = 'https://api.themoviedb.org/3/';
 const POPULAR_MOVIE_URL = 'discover/movie?language=fr&sort_by=popularity.desc&include_adult=false&append_to_response=images';
 const API_KEY = 'api_key=b1bb009f89a909c0ae0b65bc17104e0e';
+const SEARCH_URL = 'search/movie?language=fr&include_adult=false';
+const undefind = "undefined";
 
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { movieList: {}, currentMovie: {} };
+    this.state = { movieList: {}, currentMovie: {}, noVideo: ''};
   }
 
   componentWillMount() {
@@ -32,31 +34,52 @@ class App extends Component {
 
   getVideoRes() {
     axios.get(`${API_END_POINT}movie/${this.state.currentMovie.id}?${API_KEY}&append_to_response=videos&include_adult=false`).then(function (res) {
+      if (res.data.videos && res.data.videos.results[0]) {
+        const youtubeId = res.data.videos.results[0].key;
+        let newCurrentMovie = this.state.currentMovie;
+        newCurrentMovie.videoId = youtubeId;
+        this.setState({ currentMovie: newCurrentMovie, noVideo: '' });
+      } else {
 
-      const youtubeId = res.data.videos.results[0].key;
-      let newCurrentMovie = this.state.currentMovie;
-      newCurrentMovie.videoId = youtubeId;
-      this.setState({ currentMovie: newCurrentMovie });
+        this.setState({ noVideo: 'Video indisponible !!!' });
+      }
     }.bind(this));
   }
-  onClickListItem(movie){
+  onClickListItem(movie) {
     console.log('--------');
     console.log('----je suis le App grand parent----', movie);
     console.log('--------');
-    this.setState({currentMovie : movie}, ()=>{
+    this.setState({ currentMovie: movie }, () => {
       this.getVideoRes();
+      this.setRecommandation();
     })
 
   }
-  getsearchTexta(searchTexton){
-     console.log('------------');
-     console.log('-----JE SUIS DANS APP-------');
-     console.log('------------');
-     console.log('------MON TEXTSEARCH------', searchTexton);
+  getsearchTexta(searchTexton) {
 
+    axios.get(`${API_END_POINT}${SEARCH_URL}&${API_KEY}&query=${searchTexton}`).then(function (repondre) {
+      if (repondre.data && repondre.data.results[0]) {
+        if (repondre.data.results[0].id != this.state.currentMovie.id) {
+          this.setState({ currentMovie: repondre.data.results[0] }, () => {
+            this.getVideoRes();
+            this.setRecommandation();
+          })
+        }
+      }
+      console.log('------------');
+      console.log('-----JE SUIS DANS APP-------');
+      console.log('------------');
+      console.log('------MON TEXTSEARCH------', searchTexton);
+    }.bind(this));
 
   }
+  setRecommandation() {
+    axios.get(`${API_END_POINT}movie/${this.state.currentMovie.id}/recommendations?${API_KEY}&language=fr`).then(function (res) {
+      console.log('------recomm---', res);
+      this.setState({ movieList: res.data.results.slice(0, 6) });
 
+    }.bind(this));
+}
 
   render() {
 
@@ -65,15 +88,23 @@ class App extends Component {
         return <VideoList movieList={this.state.movieList} callBack={this.onClickListItem.bind(this)} />
       }
     }
+      const VideoIndispo = () => {
+        if (this.state.noVideo != ''){
+          return <h1>{this.state.noVideo}</h1>
+      }
+    
+    }
+  
     return (
       <div>
         <div className="search-bar">
-          <SearchBar  callBacka={this.getsearchTexta.bind(this)}/>
+          <SearchBar callBacka={this.getsearchTexta.bind(this)} />
         </div>
         <div className="row">
           <div className="col-md-8">
             <Video videoId={this.state.currentMovie.videoId} />
-            <VideoDetail title={this.state.currentMovie.title} overview={this.state.currentMovie.overview} date={this.state.currentMovie.release_date}/>
+            <VideoDetail title={this.state.currentMovie.title} overview={this.state.currentMovie.overview} date={this.state.currentMovie.release_date} />
+            {VideoIndispo()}
           </div>
           <div className="col-md-4">
             {rendMoviesList()}
@@ -81,7 +112,9 @@ class App extends Component {
         </div>
       </div>
     )
-  }
+    
+    }
+
 
 }
 
